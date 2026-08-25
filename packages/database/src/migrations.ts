0060b1d22d0ee65d228774aa0da8891d80e50499
huynhlongdai <itroot2015@gmail.com>
2026-08-25 12:53:30 +0700

    Register event versioning migration

--- packages/database/src/migrations.ts
+++ packages/database/src/migrations.ts
@@ -3,6 +3,7 @@ export const DATABASE_MIGRATIONS = [
   "0002_task_engine",
   "0003_organizational_truth",
   "0004_integrity_hardening",
+  "0005_event_versioning",
 ] as const;
 
 export type DatabaseMigration = (typeof DATABASE_MIGRATIONS)[number];
