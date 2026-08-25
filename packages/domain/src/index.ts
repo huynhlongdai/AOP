b8ee713a5efa5008ec80c422c2ae67849adeec9c
huynhlongdai <itroot2015@gmail.com>
2026-08-25 12:23:49 +0700

    Export T0014 truth lifecycles

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -2,5 +2,6 @@ export * from "./errors.js";
 export * from "./organization-goal.js";
 export * from "./task-dag.js";
 export * from "./task-state.js";
+export * from "./truth-state.js";
 
 export const PACKAGE_NAME = "@aop/domain" as const;
