3f503ccd6d1aa98e1625661e5940fda635a34729
huynhlongdai <itroot2015@gmail.com>
2026-08-25 16:07:48 +0700

    Export lease recovery command handlers

--- packages/command-bus/src/index.ts
+++ packages/command-bus/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./contracts.js";
 export * from "./digest.js";
 export * from "./gateway.js";
+export * from "./lease-recovery.js";
 export * from "./task-claim.js";
 
 export const PACKAGE_NAME = "@aop/command-bus" as const;
