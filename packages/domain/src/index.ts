92a67ba2a898c319d67a19ad2749ded6ed0aad8c
huynhlongdai <itroot2015@gmail.com>
2026-08-25 12:22:25 +0700

    Export T0013 Task DAG service

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./errors.js";
 export * from "./organization-goal.js";
+export * from "./task-dag.js";
 export * from "./task-state.js";
 
 export const PACKAGE_NAME = "@aop/domain" as const;
